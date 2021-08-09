f0d8ffdbeb693c3464824e3c0c305f08122348e2
siriusK <sirius0411@gmail.com>
2021-08-10 02:44:41 +0800

    fix regex

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import spamcheck
+import spamcheck, json
 
 checker = spamcheck.SpamChecker()
 
@@ -26,3 +26,21 @@ print(checker.check({
     'uname': '路人甲',
     'content': '哇嘎奶'
 }))
+
+print(checker.check({
+    'uname': '守护臀部のV型凹位',
+    'content': 'No.10是总榜啦'
+}))
+
+# with open('30.json', 'r', encoding='utf-8') as f:
+#     data = json.load(f)
+#     for i in data['full_comments']:
+#         if 'text' in i:
+#             name = i['username']
+#             content = i['text']
+#             result = checker.check({
+#                 'uname': name,
+#                 'content': content
+#             })
+#             if result['level'] > 0:
+#                 print(result['level'], name, result['signature'])
